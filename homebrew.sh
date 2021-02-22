c7882c8259c12b06f79955811003e294a374e2da
Brian Pearce <brian.o.pearce@gmail.com>
2021-02-22 12:20:09 +0100

    Add new initial brew packages

--- homebrew.sh
+++ homebrew.sh
@@ -4,14 +4,19 @@ brew update
 # Update formula
 brew upgrade
 
-# Add eepository
+# Add repository
 brew tap heroku/brew
 
 # brew
+brew install awscli
+brew install dnsmasq
+brew install docker-compose
+brew install doctl
 brew install git
 brew install gpg2
 brew install heroku
 brew install hub
+brew install kubernetes-cli
 brew install nvim
 brew install pinentry
 brew install postgresql
