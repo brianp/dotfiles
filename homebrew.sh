9fed0cd8785083866907e523fe39fda5b7e8e8dc
brianp <brian.o.pearce@gmail.com>
2024-03-25 14:38:50 +0100

    update brew deps

--- homebrew.sh
+++ homebrew.sh
@@ -5,7 +5,8 @@ brew update
 brew upgrade
 
 # Add repository
-brew tap heroku/brew
+# brew tap heroku/brew
+# brew install heroku
 
 # brew
 brew install awscli
@@ -14,10 +15,12 @@ brew install docker-compose
 brew install doctl
 brew install git
 brew install gpg2
-brew install heroku
 brew install hub
 brew install kubernetes-cli
+brew install neovim
 brew install nvim
+brew install openssl
+brew install openssl@1.1
 brew install pinentry
 brew install postgresql
 brew install python
@@ -25,5 +28,19 @@ brew install rbenv
 brew install reattach-to-user-namespace
 brew install redis
 brew install ripgrep
+brew install ruby-build
 brew install tmux
 brew install wget
+
+brew install cmake
+brew install protofbuf
+brew install tor
+brew install automake
+
+# brew install yarn
+# brew install nvm
+
+brew tap leoafarias/fvm
+brew install fvm
+
+brew install ffmpeg
